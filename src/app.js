8214328822a34545d1ae1482107764427f1bdde3
liuying <liuyinghpu@163.com>
2018-08-28 23:30:44 +0800

    chai 单元测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,20 @@ new Vue({
         loading3: false
     }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'shezhi'
+        }
+    })
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-shezhi')
+}
