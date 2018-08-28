d41d2dfb9a1513518314386123333d1f65395565
liuying <liuyinghpu@163.com>
2018-08-28 23:14:35 +0800

    第一个v-button 不需要 margin-left -1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -20,7 +20,9 @@
         display: inline-flex;
         > .v-button{
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
             &:first-child{border-top-left-radius: var(--border-radius);
             border-bottom-left-radius: var(--border-radius)}
             &:last-child{border-top-right-radius: var(--border-radius);
