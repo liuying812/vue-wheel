9fc234cd5d335ef6e5d2d304c85c9bd5a8f9f8d7
liuying <liuyinghpu@163.com>
2018-08-29 18:13:43 +0800

    row 和 col 的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
+
 
 Vue.component('v-button', Button)
 Vue.component('v-icon', Icon)
 Vue.component('v-button-group', ButtonGroup)
 Vue.component('v-input', Input)
+Vue.component('v-row', Row)
+Vue.component('v-col', Col)
 
 new Vue({
     el:'#app',
