e3610b3a9412989ff17571f9a7f07b47c561cf77
liuying <liuyinghpu@163.com>
2018-08-30 19:06:52 +0800

    添加 disabled item

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes">
         <slot></slot>
     </div>
 </template>
@@ -30,12 +30,14 @@
         computed: {
             classes(){
                 return {
-                    active: this.active
+                    active: this.active,
+                    disabled: this.disabled
                 }
             }
         },
         methods:{
-            xxx() {
+            onClick() {
+                if(this.disabled) { return }
                 this.eventBus.$emit('update:selected',this.name, this)
             }
         }
@@ -43,6 +45,7 @@
 </script>
 <style lang="scss" scoped>
     $blue: blue;
+    $disabled-text-color: #ddd;
     .tabs-item{
         flex-shrink: 0;
         padding: 0 2em;
@@ -54,5 +57,9 @@
             color: $blue;
             font-weight: bold;
          }
+        &.disabled{
+            color: $disabled-text-color;
+            cursor: not-allowed;
+        }
     }
 </style>
