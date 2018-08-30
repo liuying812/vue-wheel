254be4abda9544bc5e925b13e343389c0e51f084
liuying <liuyinghpu@163.com>
2018-08-30 12:52:50 +0800

    基本实现row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,14 +1,32 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+        <div style="border:1px solid green; height:100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
     export default {
         name: "vue-wheel-col",
         props:{
-            span: Number
+            span: Number,
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter:0   //默认值是0，从row中传入
+            }
+        },
+        created() {
+            console.log('col created')
+        },
+        mounted(){
+            console.log('col mounted')
         }
+
     }
 </script>
 <style lang="scss" scoped>
@@ -16,7 +34,7 @@
         height: 100px;
         background: grey;
         width:50%;
-        border: 1px solid red;
+       /* border: 1px solid red;*/
 
         $class: col-;
         @for $n from 1 through 24{
@@ -24,5 +42,13 @@
                 width: ($n / 24) * 100%;
             }
         }
+
+        $class: offset-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+
     }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,9 +1,27 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 <script>
+    export default {
+        name: 'vue-wheel-row',
+        props:{
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        created (){
+            console.log('row created')
+        },
+        mounted(){
+            console.log('row mounted')
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
+
+    }
 
 </script>
 <style lang="scss" scoped>
