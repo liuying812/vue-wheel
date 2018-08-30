db2cf829d59581b30d1641f0aafc04ccc7974473
liuying <liuyinghpu@163.com>
2018-08-30 13:32:16 +0800

    重构 row col 代码

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+    <div class="col" :class="colClass"
+         :style="colStyle">
         <div style="border:1px solid green; height:100px;">
             <slot></slot>
         </div>
@@ -15,24 +15,32 @@
                 type: [Number, String]
             }
         },
-        data() {
+        data() {//只在created之后读一次值
             return {
                 gutter:0   //默认值是0，从row中传入
             }
         },
-        created() {
-            console.log('col created')
-        },
-        mounted(){
-            console.log('col mounted')
+        computed:{//随着变量值的变化而变化
+            colClass(){
+              let {span , offset} = this
+              return [
+                  span && `col-${span}`,
+                  offset && `offset-${offset}`
+              ]
+            },
+            colStyle() {
+              return {
+                  paddingLeft: this.gutter / 2 + 'px',
+                  paddingRight: this.gutter / 2 + 'px'
+              }
+             }
         }
-
     }
 </script>
 <style lang="scss" scoped>
     .col{
         height: 100px;
-        background: grey;
+       /* background: grey;*/
         width:50%;
        /* border: 1px solid red;*/
 
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -11,16 +11,17 @@
                 type: [Number, String]
             }
         },
-        created (){
-            console.log('row created')
+        computed: {
+            rowStyle() {
+                let {gutter} = this
+                return {marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}
+            }
         },
         mounted(){
-            console.log('row mounted')
             this.$children.forEach((vm) => {
                 vm.gutter = this.gutter
             })
         }
-
     }
 
 </script>
