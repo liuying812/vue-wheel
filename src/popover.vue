e5b19b6b5f81e3b96e7e274c7e8eaff89af1ab5a
liuying <liuyinghpu@163.com>
2018-08-31 11:41:35 +0800

    完善popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" ref="contentWrapper" v-if="visible" @click.stop>
+    <div class="popover" @click.stop="onClick">
+        <div class="content-wrapper" ref="contentWrapper" v-if="visible" >
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -18,23 +18,30 @@
             console.log(this.$refs.triggerWrapper);
         },
         methods: {
-            xxx(){
-                this.visible  = !this.visible
-                if(this.visible === true){
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                         let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                         this.$refs.contentWrapper.style.left = left + window.screenX + 'px'
-                         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                         let eventHandler = () => {
-                               this.visible = false
-                               console.log('document 隐藏 popover');
-                               document.removeEventListener('click',eventHandler)
-                         }
-                           document.addEventListener('click',eventHandler)
+            positionContent(){
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.screenX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            listenToDocument(){
+                let eventHandler = (e) => {
+                    if (!this.$refs.contentWrapper.contains(e.target)) {
+                        this.visible = false
+                        document.removeEventListener('click', eventHandler)
+                    }
+                }
+                document.addEventListener('click',eventHandler)
+            },
+            onClick(event){
+                if(this.$refs.triggerWrapper.contains(event.target)){
+                    this.visible  = !this.visible
+                    if(this.visible === true){
+                        this.$nextTick(() => {
+                            this.positionContent()
+                            this.listenToDocument()
                         })
-                 }else {
-                    console.log('vm 隐藏 popover')
+                    }
                 }
             }
         }
