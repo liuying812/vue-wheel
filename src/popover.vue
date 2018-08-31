23dd2d46500850a021d171223fdedcfedcbca53b
liuying <liuyinghpu@163.com>
2018-08-31 11:06:28 +0800

    解决当容器有overflow:hidden造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div class="content-wrapper" ref="contentWrapper" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 <script>
@@ -12,16 +14,23 @@
         data() {
             return {visible: false}
         },
+        mounted(){
+            console.log(this.$refs.triggerWrapper);
+        },
         methods: {
             xxx(){
                 this.visible  = !this.visible
                 if(this.visible === true){
                     this.$nextTick(() => {
-                           let eventHandler = () => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                         let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                         this.$refs.contentWrapper.style.left = left + window.screenX + 'px'
+                         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                         let eventHandler = () => {
                                this.visible = false
                                console.log('document 隐藏 popover');
                                document.removeEventListener('click',eventHandler)
-                           }
+                         }
                            document.addEventListener('click',eventHandler)
                         })
                  }else {
@@ -36,12 +45,11 @@
         display: inline-block;
         vertical-align: top;
         position:relative;
-        .content-wrapper{
-            position: absolute;
-            bottom:100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow:  0 0 3px rgba(0,0,0,0.5);
-        }
+    }
+    .content-wrapper{
+        position: absolute;
+        border: 1px solid red;
+        box-shadow:  0 0 3px rgba(0,0,0,0.5);
+        transform : translateY(-100%);  //为了展示在上方
     }
 </style>
